e82c35bead9d4645d85344934fbf9dbd5259b17b
Carlos Eduardo <anjoazrael10@gmail.com>
2022-09-24 15:59:11 -0300

    Update index.ts

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,1 +1,1 @@
-export const URL_BASE_API: string = "http://localhost:3000"; // "https://nlw-api-nest.herokuapp.com";
+export const URL_BASE_API: string = "https://nlw-api-nest.herokuapp.com"; //"http://localhost:3000"; 
